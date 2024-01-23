72d037778f6ff7e452750aef008af084f84853d3
ZoZo8818 <148791533+ZoZo8818@users.noreply.github.com>
2024-01-23 13:05:21 +0000

    refactor category controller to be more efficient

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,51 +1,109 @@
-const formidable = require('formidable');
+// const formidable = require('formidable');
 
+// const { category } = require('../models');
+
+// const form = formidable({ multiples: true });
+
+// const createCategoryHandler = (req, res) => {
+//   form.parse(req, (err, fields, files) => {
+//     category.create({
+//       name: fields.name,
+//     }).then((category) => {
+//       return res.status(201).json({
+//         message: 'Category created successfully',
+//         data: category
+//       });
+//     }).catch((error) => {
+//       return res.status(400).json({ error: error.message });
+//     });
+//   });
+// };
+
+// const getAllCategoryHandler = (req, res) => {
+//   category.findAll().then((categories) => {
+//     return res.status(200).json({
+//       message: 'Get all categories',
+//       data: categories
+//     });
+//   }).catch((error) => {
+//     return res.status(500).json({ error: error.message });
+//   });
+// };
+
+// const deleteCategoryHandler = (req, res) => {
+//   category.findByPk(req.params.id).then((category) => {
+//     if (!category) {
+//       return res.status(404).json({
+//         message: 'Category not found'
+//       });
+//     }
+
+//     category.destroy().then(() => {
+//       return res.status(200).json({
+//         message: 'Category deleted successfully',
+//       });
+//     }).catch((error) => {
+//       return res.status(400).json({ error: error.message });
+//     });
+//   });
+// };
+
+// module.exports = {
+//   createCategoryHandler,
+//   getAllCategoryHandler,
+//   deleteCategoryHandler
+// };
+
+const formidable = require('formidable');
 const { category } = require('../models');
 
 const form = formidable({ multiples: true });
 
-const createCategoryHandler = (req, res) => {
-  form.parse(req, (err, fields, files) => {
-    category.create({
-      name: fields.name,
-    }).then((category) => {
-      return res.status(201).json({
-        message: 'Category created successfully',
-        data: category
-      });
-    }).catch((error) => {
-      return res.status(400).json({ error: error.message });
+const createCategoryHandler = async (req, res) => {
+  try {
+    const { name } = await form.parse(req);
+    const createdCategory = await category.create({ name });
+
+    return res.status(201).json({
+      message: 'Category created successfully',
+      data: createdCategory
     });
-  });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
 };
 
-const getAllCategoryHandler = (req, res) => {
-  category.findAll().then((categories) => {
+const getAllCategoryHandler = async (req, res) => {
+  try {
+    const categories = await category.findAll();
+
     return res.status(200).json({
       message: 'Get all categories',
       data: categories
     });
-  }).catch((error) => {
+  } catch (error) {
     return res.status(500).json({ error: error.message });
-  });
+  }
 };
 
-const deleteCategoryHandler = (req, res) => {
-  category.findByPk(req.params.id).then((category) => {
-    if (!category) {
+const deleteCategoryHandler = async (req, res) => {
+  try {
+    const foundCategory = await category.findByPk(req.params.id);
+
+    if (!foundCategory) {
       return res.status(404).json({
         message: 'Category not found'
       });
     }
 
-    category.destroy().then(() => {
-      return res.status(200).json({
-        message: 'Category deleted successfully',
-      });
-    }).catch((error) => {
-      return res.status(400).json({ error: error.message });
+    await foundCategory.destroy();
+
+    return res.status(200).json({
+      message: 'Category deleted successfully',
     });
-  });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = {
@@ -53,3 +111,4 @@ module.exports = {
   getAllCategoryHandler,
   deleteCategoryHandler
 };
+
